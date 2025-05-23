ab953901add5404640eb1f12eccd3530d1349aff
andrei-lucian <lucianandrei12@gmail.com>
2025-05-23 17:11:30 +0200

    Latest changes

--- app/views/dropdown.py
+++ app/views/dropdown.py
@@ -1,12 +1,12 @@
 from dash import html, dcc
 
-class CountryDropdown(html.Div):
+class Dropdown(html.Div):
     def __init__(self, name, df, value_col="country"):
         self.html_id = name.lower().replace(" ", "-")
         self.df = df
         self.value_col = value_col
 
-        options = [{"label": country, "value": country} for country in df[value_col]]
+        options = df
 
         super().__init__(
             className="graph_card",
@@ -17,7 +17,7 @@ class CountryDropdown(html.Div):
                 "marginBottom": "15px"
             },
             children=[
-                html.Label(f"Select {name}", style={
+                html.Label(f"{name}", style={
                     "color": "white",
                     "marginBottom": "5px",
                     "display": "block"
@@ -25,7 +25,7 @@ class CountryDropdown(html.Div):
                 dcc.Dropdown(
                     id=self.html_id,
                     options=options,
-                    value=options[0]["value"],
+                    value=options[0],
                     clearable=False,
                     style={
                         "backgroundColor": "#1e1e1e",
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from jbi100_app.main import app
-from jbi100_app.config import *
-from jbi100_app.views.map import *
-from jbi100_app.views.drop_down import *
+from app.main import app
+from app.load_df import *
+from app.views.map import *
+from app.views.dropdown import *
+from app.views.barchart import *
+
+
 from dash import html
 from dash.dependencies import Input, Output
 import json
@@ -22,10 +25,22 @@ if __name__ == '__main__':
 		"value": [300, 150, 220, 180, 250, 500, 450, 170, 130]
 	}
 
-	df = pd.DataFrame(data)
+	data_df = pd.DataFrame(data)
+	merged = full_df
+	print(merged.columns)
+	unique_genres_list = unique_genres
 
-	choropleth_component = ChoroplethMap("World Map", df)
-	country_dropdown = CountryDropdown("Country", df)
+	choropleth_component = ChoroplethMap("World Map", data_df)
+
+	genre_dropdown = Dropdown("Genre Dropdown", unique_genres_list)
+
+	bar_chart = BarChartCard(
+		name="Top Polarizing Movies (by Rating Std Dev)",
+		df=merged,
+		x_col="RatingStd",
+		y_col="Title",
+		top_n=10
+	)
 
 	# --- Styles ---
 	sidebar_style = {
@@ -61,10 +76,9 @@ if __name__ == '__main__':
 		html.Div(id="top", className="top-bar", children=None),
 
 		html.Div(id="main-content", style=main_content_style, children=[
-			html.Div(id="left-column", style=sidebar_style, children=country_dropdown),
+			html.Div(id="left-column", style=sidebar_style, children=[genre_dropdown, bar_chart]),
 
 			html.Div(id="center-column", style=center_style, children=[
-				choropleth_component,
 
 				html.Div(id="bottom-content", style=bottom_content_style, children=[
 					html.P("This area can be used for tables, insights, etc.")
@@ -72,5 +86,13 @@ if __name__ == '__main__':
 			])
 		])
 	])
+
+
+	@app.callback(
+		Output(bar_chart.html_id, 'figure'),
+		Input(genre_dropdown.html_id, 'value'),
+		)
+	def update_barchart(dropdown_val):
+		return bar_chart.update(dropdown_val)
 
 	app.run_server(debug=True, dev_tools_ui=True)
