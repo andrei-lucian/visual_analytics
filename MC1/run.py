9fc1070088e5d8c46c2c08bc50fbfb632e445e97
andrei-lucian <lucianandrei12@gmail.com>
2025-06-08 15:29:08 +0200

    Fix NLTK download issues

--- MC1/run.py
+++ MC1/run.py
@@ -1,13 +1,12 @@
 from main import app
+import nltk
+
+# Download required NLTK resources early, before anything else uses them
+nltk.download("punkt")
+nltk.download("vader_lexicon")
+
 from widgets.layout import *
 from callbacks.callbacks import register_callbacks
-
-from dash import html
-from dash.dependencies import Input, Output
-import pandas as pd
-import sys
-import os
-import nltk
 
 if __name__ == "__main__":
 
@@ -15,9 +14,6 @@ if __name__ == "__main__":
     This is the main layout of the webpage, its children are then sub divided
     into further html layouts
     """
-    nltk.download("punkt")
-    nltk.download("vader_lexicon")
-
     app.layout = create_layout()
     register_callbacks(app)
 
